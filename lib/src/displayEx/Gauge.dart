437f00965beb71ff854e5d36707edaf3f8075f65
Bernhard Pichler <bernhard.robert.pichler@gmail.com>
2012-10-24 22:10:18 +0200

    fixed bug in Gauge

--- lib/src/displayEx/Gauge.dart
+++ lib/src/displayEx/Gauge.dart
@@ -7,10 +7,9 @@ class Gauge extends DisplayObject
   static const String DIRECTION_DOWN = 'DIRECTION_DOWN';
   static const String DIRECTION_LEFT = 'DIRECTION_LEFT';
 
-  BitmapData bitmapData;
-
   String _direction;
   num _ratio;
+  BitmapData _bitmapData;
   Rectangle _clipRectangle;
 
   Gauge(BitmapData bitmapData, [String direction = DIRECTION_LEFT])
@@ -20,10 +19,9 @@ class Gauge extends DisplayObject
       throw new ArgumentError('Invalid Gauge direction!');
     }
 
-    this.bitmapData = bitmapData;
-
     _direction = direction;
     _ratio = 1.0;
+    _bitmapData = bitmapData;
     _clipRectangle = new Rectangle.zero();
 
     _updateMask();
@@ -34,7 +32,7 @@ class Gauge extends DisplayObject
 
   num get ratio => _ratio;
 
-  void set ratio(num value)
+  set ratio(num value)
   {
     if (value < 0.0) value = 0.0;
     if (value > 1.0) value = 1.0;
@@ -45,43 +43,60 @@ class Gauge extends DisplayObject
 
   //-------------------------------------------------------------------------------------------------
 
+  BitmapData get bitmapData => _bitmapData;
+
+  set bitmapData(BitmapData value)
+  {
+    _bitmapData = value;
+    _updateMask();
+  }
+
+  //-------------------------------------------------------------------------------------------------
+
   _updateMask()
   {
-    if (bitmapData == null)
-      return;
-
-    int bdWidth = bitmapData.width;
-    int bdHeight = bitmapData.height;
-
-    switch (_direction)
+    if (_bitmapData == null)
     {
-      case DIRECTION_LEFT:
-        _clipRectangle.left = ((1.0 - _ratio) * bdWidth).toInt();
-        _clipRectangle.top = 0;
-        _clipRectangle.right = bdWidth;
-        _clipRectangle.bottom = bdHeight;
-        break;
+      _clipRectangle.left = 0;
+      _clipRectangle.top = 0;
+      _clipRectangle.right = 0;
+      _clipRectangle.bottom = 0;
 
-      case DIRECTION_UP:
-        _clipRectangle.left = 0;
-        _clipRectangle.top = ((1.0 - _ratio) * bdHeight).toInt();
-        _clipRectangle.right = bdWidth;
-        _clipRectangle.bottom = bdHeight;
-        break;
+    } else {
 
-      case DIRECTION_RIGHT:
-        _clipRectangle.left = 0;
-        _clipRectangle.top = 0;
-        _clipRectangle.right = (_ratio * bdWidth).toInt();
-        _clipRectangle.bottom = bdHeight;
-        break;
+      int bdWidth = _bitmapData.width;
+      int bdHeight = _bitmapData.height;
 
-      case DIRECTION_DOWN:
-        _clipRectangle.left = 0;
-        _clipRectangle.top = 0;
-        _clipRectangle.right = bdWidth;
-        _clipRectangle.bottom = (_ratio * bdHeight).toInt();
-        break;
+      switch (_direction)
+      {
+        case DIRECTION_LEFT:
+          _clipRectangle.left = ((1.0 - _ratio) * bdWidth).toInt();
+          _clipRectangle.top = 0;
+          _clipRectangle.right = bdWidth;
+          _clipRectangle.bottom = bdHeight;
+          break;
+
+        case DIRECTION_UP:
+          _clipRectangle.left = 0;
+          _clipRectangle.top = ((1.0 - _ratio) * bdHeight).toInt();
+          _clipRectangle.right = bdWidth;
+          _clipRectangle.bottom = bdHeight;
+          break;
+
+        case DIRECTION_RIGHT:
+          _clipRectangle.left = 0;
+          _clipRectangle.top = 0;
+          _clipRectangle.right = (_ratio * bdWidth).toInt();
+          _clipRectangle.bottom = bdHeight;
+          break;
+
+        case DIRECTION_DOWN:
+          _clipRectangle.left = 0;
+          _clipRectangle.top = 0;
+          _clipRectangle.right = bdWidth;
+          _clipRectangle.bottom = (_ratio * bdHeight).toInt();
+          break;
+      }
     }
   }
 
@@ -90,8 +105,8 @@ class Gauge extends DisplayObject
 
   Rectangle getBoundsTransformed(Matrix matrix, [Rectangle returnRectangle = null])
   {
-    int width = (bitmapData != null) ? bitmapData.width : 0;
-    int height = (bitmapData != null) ? bitmapData.height : 0;
+    int width = (_bitmapData != null) ? _bitmapData.width : 0;
+    int height = (_bitmapData != null) ? _bitmapData.height : 0;
 
     return _getBoundsTransformedHelper(matrix, width, height, returnRectangle);
   }
@@ -100,7 +115,7 @@ class Gauge extends DisplayObject
 
   DisplayObject hitTestInput(num localX, num localY)
   {
-    if (bitmapData != null && localX >= 0.0 && localY >= 0.0 && localX < bitmapData.width && localY < bitmapData.height)
+    if (_bitmapData != null && localX >= 0.0 && localY >= 0.0 && localX < _bitmapData.width && localY < _bitmapData.height)
       return this;
 
     return null;
@@ -110,8 +125,8 @@ class Gauge extends DisplayObject
 
   void render(RenderState renderState)
   {
-    if (bitmapData != null)
-        bitmapData.renderClipped(renderState, _clipRectangle);
+    if (_bitmapData != null)
+        _bitmapData.renderClipped(renderState, _clipRectangle);
   }
 
 
