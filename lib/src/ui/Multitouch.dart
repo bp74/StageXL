d48e411d1184565e861051924e372b9958d08577
Bernhard <bernhard.robert.pichler@gmail.com>
2013-04-03 15:17:47 +0200

    updated TouchEvent detection

--- lib/src/ui/Multitouch.dart
+++ lib/src/ui/Multitouch.dart
@@ -11,74 +11,26 @@ class Multitouch
 {
   static bool _initialized = false;
   static bool _supportsGestureEvents = false;
-  static bool _supportsTouchEvents = false;
-  static int _maxTouchPoints = 0;
+  static bool _supportsTouchEvents = html.TouchEvent.supported;
+  static int _maxTouchPoints = html.TouchEvent.supported ? 10 : 0;
+  
   static List<String> _supportedGestures = [];
   static String _inputMode =  MultitouchInputMode.NONE;
 
   static StreamController<String> _inputModeChangedEvent = new StreamController<String>();
   static Stream<String> _onInputModeChanged = _inputModeChangedEvent.stream.asBroadcastStream();
+  
+  //------------------------------------------------------------------
 
-  static _initialize()
-  {
-    if (_initialized == false)
-    {
-      _initialized = true;
-
-      var ua = html.window.navigator.userAgent;
-
-      // ToDo: feature detection using the userAgent is a bad idea in general.
-      // Unfortunately there are no other good options right now :/
-      // Maybe do some JS-interop?
-
-      if (ua.contains("iPhone") || ua.contains("iPad") || ua.contains("iPod")) {
-        _supportsTouchEvents = true;
-        _maxTouchPoints = 10;
-      }
-
-      if (ua.contains("Android")) {
-        _supportsTouchEvents = true;
-        _maxTouchPoints = 10;
-      }
-
-      if (ua.contains("IEMobile")) {
-        _supportsTouchEvents = true;
-        _maxTouchPoints = 10;
-      }
-    }
-  }
+  static bool get supportsGestureEvents => _supportsGestureEvents;
+  static bool get supportsTouchEvents => _supportsTouchEvents;
+  static int get maxTouchPoints => _maxTouchPoints;
+  static List<String> get supportedGestures => _supportedGestures;
 
   //------------------------------------------------------------------
 
-  static bool get supportsGestureEvents {
-    _initialize();
-    return _supportsGestureEvents;
-  }
-
-  static bool get supportsTouchEvents {
-    _initialize();
-    return _supportsTouchEvents;
-  }
-
-  static int get maxTouchPoints {
-    _initialize();
-    return _maxTouchPoints;
-  }
-
-  static List<String> get supportedGestures {
-    _initialize();
-    return _supportedGestures;
-  }
-
-  //------------------------------------------------------------------
-
-  static String get inputMode {
-    _initialize();
-    return _inputMode;
-  }
-
+  static String get inputMode => _inputMode;
   static set inputMode(String value) {
-    _initialize();
     _inputMode = value;
     _inputModeChangedEvent.add(_inputMode);
   }
