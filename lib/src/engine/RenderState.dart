4af3dbb916130fb05372e89e9f7ff698811ed2cf
Bernhard <bernhard.robert.pichler@gmail.com>
2013-04-19 16:25:17 +0200

    RenderState reworked
    some optimizations still pending

--- lib/src/engine/RenderState.dart
+++ lib/src/engine/RenderState.dart
@@ -1,30 +1,41 @@
 part of stagexl;
 
+class _ContextState {
+  final Matrix matrix = new Matrix.fromIdentity();
+  double alpha = 1.0;
+  String compositeOperation = CompositeOperation.SOURCE_OVER;
+
+  _ContextState _nextContextState;
+  
+  _ContextState get nextContextState {
+    if (_nextContextState == null) _nextContextState = new _ContextState();
+    return _nextContextState;
+  }
+}
+
 class RenderState {
   
   final CanvasRenderingContext2D _context;
-  final List<Matrix> _matrices = new List<Matrix>(100);
-  final List<double> _alphas = new List<double>(100);
-  final List<String> _composite = new List<String>(100);
-  int _depth = 0;
+  
   num _currentTime = 0.0;
   num _deltaTime = 0.0;
-
+  _ContextState _firstContextState;
+  _ContextState _currentContextState;
+  
   RenderState.fromCanvasRenderingContext2D(CanvasRenderingContext2D context, [Matrix matrix]) :
     _context = context {
-
-    for(int i = 0; i < 100; i++) {
-      _matrices[i] = new Matrix.fromIdentity();
-      _alphas[i] = 1.0;
-      _composite[i] = CompositeOperation.SOURCE_OVER;
-    }
-   
-    var m = _matrices[0];
-    if (matrix != null) m.copyFrom(matrix);
     
+    _firstContextState = new _ContextState(); 
+    _currentContextState = _firstContextState;
+    
+    if (matrix != null) {
+      _firstContextState.matrix.copyFrom(matrix);
+    }
+
+    var m = _firstContextState.matrix;
     _context.setTransform(m.a, m.b, m.c, m.d, m.tx, m.ty);
-    _context.globalAlpha = _alphas[0];
-    _context.globalCompositeOperation = _composite[0];
+    _context.globalAlpha = 1.0;
+    _context.globalCompositeOperation = CompositeOperation.SOURCE_OVER;
   }
 
   //-------------------------------------------------------------------------------------------------
@@ -38,87 +49,84 @@ class RenderState {
 
   void reset([Matrix matrix, num currentTime, num deltaTime]) {
     
-    _depth = 0;
     _currentTime = ?currentTime ? currentTime : 0.0;
     _deltaTime = ?deltaTime ? deltaTime : 0.0;
-
-    var m = _matrices[0];
-    if (matrix != null) m.copyFrom(matrix);
+    _currentContextState = _firstContextState;
     
+    if (matrix != null) {
+      _firstContextState.matrix.copyFrom(matrix);
+    }
+    
+    var m = _firstContextState.matrix;
     _context.setTransform(m.a, m.b, m.c, m.d, m.tx, m.ty);
-    _context.globalAlpha = _alphas[0];
-    _context.globalCompositeOperation = _composite[0];
-    
+    _context.globalAlpha = 1.0;
+    _context.globalCompositeOperation = CompositeOperation.SOURCE_OVER;
     _context.clearRect(0, 0, _context.canvas.width, _context.canvas.height);
   }
  
   //-------------------------------------------------------------------------------------------------
 
   void renderDisplayObject(DisplayObject displayObject) {
-
+    
     var matrix = displayObject._transformationMatrix;
     var alpha = displayObject._alpha;
     var mask = displayObject._mask;
     var shadow = displayObject._shadow;
-    var cache = displayObject._cache;
     var composite = displayObject._compositeOperation;
+    var cache = displayObject._cache;
     
-    var d1 = _depth;
-    var d2 = _depth + 1;
-    var m1 = _matrices[d1];
-    var m2 = _matrices[d2];
-    var a1 = _alphas[d1];
-    var a2 = _alphas[d2] = alpha * a1;
-    var c1 = _composite[d1];
-    var c2 = _composite[d2] = ((composite == null) ? c1 : composite);
+    var cs1 = _currentContextState as _ContextState;
+    var cs2 = _currentContextState.nextContextState as _ContextState;
     
-    m2.copyFromAndConcat(matrix, m1);
-    _depth = d2;
+    _currentContextState = cs2;
     
-    // save context 
+    var nextMatrix = cs2.matrix;
+    var nextAlpha = cs1.alpha * alpha;
+    var nextCompositeOperation = (composite != null) ? composite : cs1.compositeOperation;
+    
+    nextMatrix.copyFromAndConcat(matrix, cs1.matrix);
+    cs2.alpha = nextAlpha;
+    cs2.compositeOperation = nextCompositeOperation;
+    
+    // apply Mask and Shadow
     if (mask != null || shadow != null) {
+      
       _context.save();      
-    }
-    
-    // apply Mask
-    if (mask != null) {
-      
-      if (mask.targetSpace == null) {
-        matrix = m2;
-      } else if (identical(mask.targetSpace, displayObject)) {
-        matrix = m2;
-      } else if (identical(mask.targetSpace, displayObject.parent)) {
-        matrix = m1;
-      } else {
-        matrix = mask.targetSpace.transformationMatrixTo(displayObject);
-        if (matrix == null) matrix = _identityMatrix; else matrix.concat(m2);
-      }
-      
-      mask.render(this, matrix);
-    }
 
-    // apply shadow
-    if (shadow != null) {
-      
-      if (shadow.targetSpace == null) {
-        matrix = m2;
-      } else if (identical(shadow.targetSpace, displayObject)) {
-        matrix = m2;
-      } else if (identical(shadow.targetSpace, displayObject.parent)) {
-        matrix = m1;
-      } else {
-        matrix = shadow.targetSpace.transformationMatrixTo(displayObject);
-        if (matrix == null) matrix = _identityMatrix; else matrix.concat(m2);
+      if (mask != null) {
+        if (mask.targetSpace == null) {
+          matrix = cs2.matrix;
+        } else if (identical(mask.targetSpace, displayObject)) {
+          matrix = cs2.matrix;
+        } else if (identical(mask.targetSpace, displayObject.parent)) {
+          matrix = cs1.matrix;
+        } else {
+          matrix = mask.targetSpace.transformationMatrixTo(displayObject);
+          if (matrix == null) matrix = _identityMatrix; else matrix.concat(cs2.matrix);
+        }
+        mask.render(this, matrix);
       }
-      
-      shadow.render(this, matrix);
+  
+      if (shadow != null) {
+        if (shadow.targetSpace == null) {
+          matrix = cs2.matrix;
+        } else if (identical(shadow.targetSpace, displayObject)) {
+          matrix = cs2.matrix;
+        } else if (identical(shadow.targetSpace, displayObject.parent)) {
+          matrix = cs1.matrix;
+        } else {
+          matrix = shadow.targetSpace.transformationMatrixTo(displayObject);
+          if (matrix == null) matrix = _identityMatrix; else matrix.concat(cs2.matrix);
+        }
+        shadow.render(this, matrix);
+      }
     }
 
     // render DisplayObject
-    
-    _context.setTransform(m2.a, m2.b, m2.c, m2.d, m2.tx, m2.ty);
-    _context.globalAlpha = a2;
-    _context.globalCompositeOperation = c2;
+    var m = nextMatrix;
+    _context.setTransform(m.a, m.b, m.c, m.d, m.tx, m.ty);
+    _context.globalCompositeOperation = nextCompositeOperation;
+    _context.globalAlpha = nextAlpha;  
     
     if (cache != null) {
       cache.render(this);
@@ -131,7 +139,7 @@ class RenderState {
       _context.restore();
     }
 
-    _depth = d1;
+    _currentContextState = cs1;
   }
 
 }
