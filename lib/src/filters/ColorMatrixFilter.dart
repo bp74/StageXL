fa27ba506c28236e4d7fb96be3384b3ca316f41e
Bernhard <bernhard.robert.pichler@gmail.com>
2013-07-07 09:02:32 +0200

    reduced ColorMatrixFilter javascript bailouts

--- lib/src/filters/ColorMatrixFilter.dart
+++ lib/src/filters/ColorMatrixFilter.dart
@@ -1,30 +1,29 @@
 part of stagexl;
 
 class ColorMatrixFilter extends BitmapFilter {
-  
-  final List<num> _matrix;
 
-  ColorMatrixFilter(List<num> matrix) :
-    _matrix = new List<num>(20) {
+  final List<num> _matrix = new List.filled(20, 0.0);
 
-    if (matrix.length != 20)
+  ColorMatrixFilter(List<num> matrix) {
+    if (matrix.length != 20) {
       throw new ArgumentError("The supplied matrix needs to be a 4 x 5 matrix.");
-    
-    for(int i = 0; i < matrix.length; i++)
-      _matrix[i] = matrix[i];
+    }
+    for(int i = 0; i < matrix.length; i++) {
+      _matrix[i] = _ensureNum(matrix[i]);
+    }
   }
 
-  ColorMatrixFilter.grayscale() : 
-    _matrix = [0.212671, 0.715160, 0.072169, 0, 0,
-               0.212671, 0.715160, 0.072169, 0, 0,
-               0.212671, 0.715160, 0.072169, 0, 0,
-               0, 0, 0, 1, 0];
+  ColorMatrixFilter.grayscale() : this([
+    0.212671, 0.715160, 0.072169, 0, 0,
+    0.212671, 0.715160, 0.072169, 0, 0,
+    0.212671, 0.715160, 0.072169, 0, 0,
+    0, 0, 0, 1, 0]);
 
-  ColorMatrixFilter.invert() :
-    _matrix = [-1,  0,  0, 0, 255,
-                0, -1,  0, 0, 255,
-                0,  0, -1, 0, 255,
-                0,  0,  0, 1, 0];
+  ColorMatrixFilter.invert() : this([
+    -1,  0,  0, 0, 255,
+     0, -1,  0, 0, 255,
+     0,  0, -1, 0, 255,
+     0,  0,  0, 1, 0]);
 
   //-------------------------------------------------------------------------------------------------
   //-------------------------------------------------------------------------------------------------
@@ -42,30 +41,32 @@ class ColorMatrixFilter extends BitmapFilter {
     //blueResult  = (a[10] * srcR) + (a[11] * srcG) + (a[12] * srcB) + (a[13] * srcA) + a[14]
     //alphaResult = (a[15] * srcR) + (a[16] * srcG) + (a[17] * srcB) + (a[18] * srcA) + a[19]
 
+    if (_matrix.length < 20) throw "dart2js_hint";
+
+    int a00 = (_matrix[00] * 65536).round();
+    int a01 = (_matrix[01] * 65536).round();
+    int a02 = (_matrix[02] * 65536).round();
+    int a03 = (_matrix[03] * 65536).round();
+    int a04 = (_matrix[04] * 65536).round();
+    int a05 = (_matrix[05] * 65536).round();
+    int a06 = (_matrix[06] * 65536).round();
+    int a07 = (_matrix[07] * 65536).round();
+    int a08 = (_matrix[08] * 65536).round();
+    int a09 = (_matrix[09] * 65536).round();
+    int a10 = (_matrix[10] * 65536).round();
+    int a11 = (_matrix[11] * 65536).round();
+    int a12 = (_matrix[12] * 65536).round();
+    int a13 = (_matrix[13] * 65536).round();
+    int a14 = (_matrix[14] * 65536).round();
+    int a15 = (_matrix[15] * 65536).round();
+    int a16 = (_matrix[16] * 65536).round();
+    int a17 = (_matrix[17] * 65536).round();
+    int a18 = (_matrix[18] * 65536).round();
+    int a19 = (_matrix[19] * 65536).round();
+
     var imageData = sourceBitmapData.getImageData(
         sourceRect.x, sourceRect.y, sourceRect.width, sourceRect.height, destinationBitmapData.pixelRatio);
-    var data = imageData.data;
-
-    int a00 = (_matrix[00] * 65536).toInt();
-    int a01 = (_matrix[01] * 65536).toInt();
-    int a02 = (_matrix[02] * 65536).toInt();
-    int a03 = (_matrix[03] * 65536).toInt();
-    int a04 = (_matrix[04] * 65536).toInt();
-    int a05 = (_matrix[05] * 65536).toInt();
-    int a06 = (_matrix[06] * 65536).toInt();
-    int a07 = (_matrix[07] * 65536).toInt();
-    int a08 = (_matrix[08] * 65536).toInt();
-    int a09 = (_matrix[09] * 65536).toInt();
-    int a10 = (_matrix[10] * 65536).toInt();
-    int a11 = (_matrix[11] * 65536).toInt();
-    int a12 = (_matrix[12] * 65536).toInt();
-    int a13 = (_matrix[13] * 65536).toInt();
-    int a14 = (_matrix[14] * 65536).toInt();
-    int a15 = (_matrix[15] * 65536).toInt();
-    int a16 = (_matrix[16] * 65536).toInt();
-    int a17 = (_matrix[17] * 65536).toInt();
-    int a18 = (_matrix[18] * 65536).toInt();
-    int a19 = (_matrix[19] * 65536).toInt();
+    var data = imageData.data as Iterable;
 
     if (_isLittleEndianSystem) {
       for(int index = 0 ; index <= data.length - 4; index += 4) {
