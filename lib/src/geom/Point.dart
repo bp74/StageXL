25380f8e92c6e9b7343a1ab6bfcc7a8462ec7dad
Bernhard <bernhard.robert.pichler@gmail.com>
2013-09-05 07:27:46 +0200

    Fixed Point.polar calculation

--- lib/src/geom/Point.dart
+++ lib/src/geom/Point.dart
@@ -1,36 +1,36 @@
 part of stagexl;
 
 class Point {
-  
+
   num _x;
   num _y;
 
-  Point(num x, num y) : _x = x, _y = y; 
+  Point(num x, num y) : _x = x, _y = y;
 
   Point.zero() : this(0, 0);
 
   Point.from(Point p) : this(p.x, p.y);
-  
+
   Point clone() => new Point(_x, _y);
 
   String toString() => "Point [x=${_x}, y=${_y}]";
-  
+
   //-------------------------------------------------------------------------------------------------
   //-------------------------------------------------------------------------------------------------
 
   static num distance(Point p1, Point p2) => p1.distanceTo(p2);
 
-  static Point interpolate(Point p1, Point p2, num f) => 
+  static Point interpolate(Point p1, Point p2, num f) =>
     new Point(p2.x + (p1.x - p2.x) * f, p2.y + (p1.y - p2.y) * f);
 
-  static Point polar(num len, num angle) => 
-    new Point(len * cos(angle), - len * sin(angle));
+  static Point polar(num len, num angle) =>
+    new Point(len * cos(angle), len * sin(angle));
 
   //-------------------------------------------------------------------------------------------------
 
   num get x => _x;
   num get y => _y;
-  
+
   num get length => sqrt(_x * _x + _y * _y);
 
   set x(num value) {
@@ -51,14 +51,14 @@ class Point {
     return new Point(_x - p.x, _y - p.y);
   }
 
-  copyFrom(Point p) { 
-    _x = p.x; 
-    _y = p.y; 
+  copyFrom(Point p) {
+    _x = p.x;
+    _y = p.y;
   }
 
-  setTo(num px, num py) { 
-    _x = px; 
-    _y = py; 
+  setTo(num px, num py) {
+    _x = px;
+    _y = py;
   }
 
   bool equals(Point p) {
