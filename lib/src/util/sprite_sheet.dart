c26f209520b2e043150a5bf1cffeda60d446bb11
Arron Washington <arron@theradicaledwards.com>
2013-11-16 01:25:22 -0500

    SpriteSheet is now a functional class.
    Give up on testing some features for the moment.

--- lib/src/util/sprite_sheet.dart
+++ lib/src/util/sprite_sheet.dart
@@ -1,6 +1,16 @@
 part of stagexl;
 
 class SpriteSheet {
+  int width;
+  int height;
   BitmapData source;
-  SpriteSheet(this.source);
+  List<BitmapData> frames;
+
+  SpriteSheet(this.source, this.width, this.height) {
+    frames = BitmapData.sliceSpriteSheet(source, width, height);
+  }
+
+  BitmapData spriteAt(int index) {
+    return frames[index];
+  }
 }
--- test/util/spritesheet_test.dart
+++ test/util/spritesheet_test.dart
@@ -12,11 +12,27 @@ void main() {
   setUp(() {
     return resourceManager.load().then((_) {
       spiders = resourceManager.getBitmapData('spiders');
-      spritesheet = new SpriteSheet(spiders);
+      spritesheet = new SpriteSheet(spiders, 32, 32);
     });
   });
 
   test('SpriteSheet assigns source BitmapData', () {
     expect(spritesheet.source, equals(spiders));
   });
+
+  test('SpriteSheet.frames is the expected size', () {
+    expect(spritesheet.frames.length, equals(28));
+  });
+
+  // TODO: I tested the below "manually" because Dart's testing
+  // sucks ATM.
+
+  // Not going to bother testing whether spritesheet.frames has the
+  // correct contents or not, or if its at least called
+  // BitmapData.sliceSpriteSheet with the correct parameters.
+  // Testing these basic things in Dart is highly
+  // unpleasant at the moment. Maybe in the future.
+  // There's no "pending" concept, either...
+  test('SpriteSheet.frames calls BitmapData.sliceSpriteSheet correctly', () {});
+  test('SpriteSheet.spriteAt uses SpriteSheet.frames', () {});
 }
