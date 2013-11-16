b6bca0b8fed2dd2f1e4d5852b352f299109b5ca0
Arron Washington <arron@theradicaledwards.com>
2013-11-15 19:33:05 -0500

    Clean up test setup.

--- test/util/spritesheet_test.dart
+++ test/util/spritesheet_test.dart
@@ -7,14 +7,16 @@ import 'dart:async';
 void main() {
   ResourceManager resourceManager = new ResourceManager();
   resourceManager.addBitmapData('spiders', '/StageXL/test/assets/spider.png');
-  resourceManager.load().then((_) {
-    
-    var spiders = resourceManager.getBitmapData('spiders');
-    var spritesheet = new SpriteSheet(spiders);
-    
-    test('SpriteSheet assigns source BitmapData', () {
-      expect(spritesheet.source, equals(spiders));
+  var spiders, spritesheet;
+
+  setUp(() {
+    return resourceManager.load().then((_) {
+      spiders = resourceManager.getBitmapData('spiders');
+      spritesheet = new SpriteSheet(spiders);
     });
-    
+  });
+
+  test('SpriteSheet assigns source BitmapData', () {
+    expect(spritesheet.source, equals(spiders));
   });
 }
