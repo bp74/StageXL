db2debd89fe0fe07ce3bee49d3041da707043074
Bernhard <bernhard.robert.pichler@gmail.com>
2013-11-17 08:27:01 +0100

    moved vector_test to a sub folder.

--- test/test_runner.dart
+++ test/test_runner.dart
@@ -1,14 +1,14 @@
 import 'package:unittest/unittest.dart';
 import 'package:unittest/html_enhanced_config.dart';
 
-import 'vector_test.dart' as vector_test;
-import 'util/spritesheet_test.dart' as spritesheet_test;
 import 'display/bitmap_data_test.dart' as bitmap_data_test;
+import 'geom/vector_test.dart' as vector_test;
+import 'util/spritesheet_test.dart' as spritesheet_test;
 
 void main() {
   useHtmlEnhancedConfiguration();
 
+  group('BitmapData tests', bitmap_data_test.main);
   group('Vector tests', vector_test.main);
   group('Spritesheet tests', spritesheet_test.main);
-  group('BitmapData tests', bitmap_data_test.main);
 }
