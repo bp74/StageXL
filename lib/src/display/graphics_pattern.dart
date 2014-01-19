4d51cb710ca81522b24989606e11f1289204604a
Bernhard <bernhard.robert.pichler@gmail.com>
2014-01-19 01:38:23 +0100

    Fixed GraphicsPattern

--- lib/src/display/graphics_pattern.dart
+++ lib/src/display/graphics_pattern.dart
@@ -36,16 +36,7 @@ class GraphicsPattern {
   Matrix get matrix => _matrix;
 
   CanvasPattern getCanvasPattern(CanvasRenderingContext2D context) {
-    // TODO: WebGL
-
-    /*
-    CanvasImageSource source = _bitmapData._source;
-    if(source is ImageElement) {
-      return context.createPatternFromImage(source, _repeatOption);
-    } else {
-      return context.createPattern(source, _repeatOption);
-    }
-    */
+    return context.createPattern(_bitmapData.renderTexture.canvas, _repeatOption);
   }
 
 }
