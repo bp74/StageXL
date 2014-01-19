e9573ebed7fa684481d4f4033e9b8e8c02ed0c98
Bernhard <bernhard.robert.pichler@gmail.com>
2014-01-19 10:45:36 +0100

    Fixed typo

--- lib/src/filters/color_matrix_filter.dart
+++ lib/src/filters/color_matrix_filter.dart
@@ -43,21 +43,24 @@ class ColorMatrixFilter extends BitmapFilter {
 
     bool isLittleEndianSystem = _isLittleEndianSystem;
 
-    int d0s0 = ((isLittleEndianSystem ? _matrix[00] : _matrix[18]) * 65536).round();
+    int d0c0 = ((isLittleEndianSystem ? _matrix[00] : _matrix[18]) * 65536).round();
     int d0c1 = ((isLittleEndianSystem ? _matrix[01] : _matrix[17]) * 65536).round();
     int d0c2 = ((isLittleEndianSystem ? _matrix[02] : _matrix[16]) * 65536).round();
     int d0c3 = ((isLittleEndianSystem ? _matrix[03] : _matrix[15]) * 65536).round();
     int d0cc = ((isLittleEndianSystem ? _matrix[04] : _matrix[19]) * 65536).round();
+
     int d1c0 = ((isLittleEndianSystem ? _matrix[05] : _matrix[13]) * 65536).round();
     int d1c1 = ((isLittleEndianSystem ? _matrix[06] : _matrix[12]) * 65536).round();
     int d1c2 = ((isLittleEndianSystem ? _matrix[07] : _matrix[11]) * 65536).round();
     int d1c3 = ((isLittleEndianSystem ? _matrix[08] : _matrix[10]) * 65536).round();
     int d1cc = ((isLittleEndianSystem ? _matrix[09] : _matrix[14]) * 65536).round();
+
     int d2c0 = ((isLittleEndianSystem ? _matrix[10] : _matrix[08]) * 65536).round();
     int d2c1 = ((isLittleEndianSystem ? _matrix[11] : _matrix[07]) * 65536).round();
     int d2c2 = ((isLittleEndianSystem ? _matrix[12] : _matrix[06]) * 65536).round();
     int d2c3 = ((isLittleEndianSystem ? _matrix[13] : _matrix[05]) * 65536).round();
     int d2cc = ((isLittleEndianSystem ? _matrix[14] : _matrix[09]) * 65536).round();
+
     int d3c0 = ((isLittleEndianSystem ? _matrix[15] : _matrix[03]) * 65536).round();
     int d3c1 = ((isLittleEndianSystem ? _matrix[16] : _matrix[02]) * 65536).round();
     int d3c2 = ((isLittleEndianSystem ? _matrix[17] : _matrix[01]) * 65536).round();
@@ -73,7 +76,7 @@ class ColorMatrixFilter extends BitmapFilter {
       int c1 = data[index + 1];
       int c2 = data[index + 2];
       int c3 = data[index + 3];
-      data[index + 0] = ((d0s0 * c0 + d0c1 * c1 + d0c2 * c2 + d0c3 * c3 + d0cc) | 0) >> 16;
+      data[index + 0] = ((d0c0 * c0 + d0c1 * c1 + d0c2 * c2 + d0c3 * c3 + d0cc) | 0) >> 16;
       data[index + 1] = ((d1c0 * c0 + d1c1 * c1 + d1c2 * c2 + d1c3 * c3 + d1cc) | 0) >> 16;
       data[index + 2] = ((d2c0 * c0 + d2c1 * c1 + d2c2 * c2 + d2c3 * c3 + d2cc) | 0) >> 16;
       data[index + 3] = ((d3c0 * c0 + d3c1 * c1 + d3c2 * c2 + d3c3 * c3 + d3cc) | 0) >> 16;
