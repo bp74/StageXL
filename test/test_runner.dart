a0cdf1a59aef66881c8cb92f794abf06d34568a2
Wilker Lucio <wilkerlucio@gmail.com>
2014-02-07 23:15:56 -0300

    added tests for point and clean the test code a bit

--- test/test_runner.dart
+++ test/test_runner.dart
@@ -5,6 +5,7 @@ import 'display/bitmap_data_test.dart' as bitmap_data_test;
 import 'engine/render_texture_quad_test.dart' as render_texture_quad_test;
 import 'events/events_test.dart' as events_test;
 import 'geom/rectangle_test.dart' as rectangle_test;
+import 'geom/point_test.dart' as point_test;
 import 'geom/vector_test.dart' as vector_test;
 import 'util/spritesheet_test.dart' as spritesheet_test;
 
@@ -15,6 +16,7 @@ void main() {
   group('RenderTextureQuad tests', render_texture_quad_test.main);
   group('Events tests', events_test.main);
   group('Rectangle tests', rectangle_test.main);
+  group('Point tests', point_test.main);
   group('Vector tests', vector_test.main);
   group('Spritesheet tests', spritesheet_test.main);
 }
