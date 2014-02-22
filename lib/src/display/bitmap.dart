20a33b50a89094c946c4c47462e81d18c04b1f55
Bernhard <bernhard.robert.pichler@gmail.com>
2014-02-22 20:09:55 +0100

    Temporary disabled Bitmap filter optimization
    Doesn't work as designed ...

--- lib/src/display/bitmap.dart
+++ lib/src/display/bitmap.dart
@@ -24,8 +24,10 @@ class Bitmap extends DisplayObject {
     if (bitmapData != null) bitmapData.render(renderState);
   }
 
+  /*
   void renderFiltered(RenderState renderState) {
     if (bitmapData != null) bitmapData.renderFiltered(renderState, this.filters);
   }
+  */
 
 }
