0140e24edda1c60b7c8a8c16a7ef3741c8814ce5
Bernhard <bernhard.robert.pichler@gmail.com>
2014-09-13 19:42:27 +0200

    Changed PerspectiveProjection constructor

--- lib/src/geom/perspective_projection.dart
+++ lib/src/geom/perspective_projection.dart
@@ -7,14 +7,14 @@ class PerspectiveProjection {
   final Matrix3D transformationMatrix3D = new Matrix3D.fromIdentity();
 
   factory PerspectiveProjection() {
-    return new PerspectiveProjection.fromDepth(10, 10000);
+    return new PerspectiveProjection.fromDepth(10000, 10);
   }
 
   // TODO: Add a more comprehensible PerspectiveProjection constructor
 
-  PerspectiveProjection.fromDepth(num distance, num depth) {
+  PerspectiveProjection.fromDepth(num depth, num scale) {
     transformationMatrix3D.setIdentity();
     transformationMatrix3D.data[10] = 2.0 / depth;
-    transformationMatrix3D.data[14] = distance / depth;
+    transformationMatrix3D.data[14] = scale / depth;
   }
 }
