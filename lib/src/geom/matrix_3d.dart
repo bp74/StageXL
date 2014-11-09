3824b6cc389603ddc518c6566e20b3892a1e5fda
Bernhard <bernhard.robert.pichler@gmail.com>
2014-11-09 10:03:06 +0100

    Specialized 2D matrix calculations in Matrix3D
    A little less work for the CPU

--- lib/src/geom/matrix_3d.dart
+++ lib/src/geom/matrix_3d.dart
@@ -355,16 +355,76 @@ class Matrix3D {
     this.copyFromAndConcat(this, matrix);
   }
 
-  void concat2D(Matrix matrix) {
-    this.copyFromAndConcat2D(this, matrix);
-  }
-
   void prepend(Matrix3D matrix) {
     this.copyFromAndConcat(matrix, this);
   }
 
+  void concat2D(Matrix matrix) {
+
+    num m00 = this.m00;
+    num m10 = this.m10;
+    num m20 = this.m20;
+    num m30 = this.m30;
+    num m01 = this.m01;
+    num m11 = this.m11;
+    num m21 = this.m21;
+    num m31 = this.m31;
+    num m03 = this.m03;
+    num m13 = this.m13;
+    num m23 = this.m23;
+    num m33 = this.m33;
+
+    num n00 = matrix.a;
+    num n10 = matrix.c;
+    num n30 = matrix.tx;
+    num n01 = matrix.b;
+    num n11 = matrix.d;
+    num n31 = matrix.ty;
+
+    _data[00] = m00 * n00 + m01 * n10 + m03 * n30;
+    _data[01] = m10 * n00 + m11 * n10 + m13 * n30;
+    _data[02] = m20 * n00 + m21 * n10 + m23 * n30;
+    _data[03] = m30 * n00 + m31 * n10 + m33 * n30;
+    _data[04] = m00 * n01 + m01 * n11 + m03 * n31;
+    _data[05] = m10 * n01 + m11 * n11 + m13 * n31;
+    _data[06] = m20 * n01 + m21 * n11 + m23 * n31;
+    _data[07] = m30 * n01 + m31 * n11 + m33 * n31;
+  }
+
   void prepend2D(Matrix matrix) {
-    this.copyFrom2DAndConcat(matrix, this);
+
+    num m00 = matrix.a;
+    num m10 = matrix.c;
+    num m30 = matrix.tx;
+    num m01 = matrix.b;
+    num m11 = matrix.d;
+    num m31 = matrix.ty;
+
+    num n00 = this.m00;
+    num n10 = this.m10;
+    num n30 = this.m30;
+    num n01 = this.m01;
+    num n11 = this.m11;
+    num n31 = this.m31;
+    num n02 = this.m02;
+    num n12 = this.m12;
+    num n32 = this.m32;
+    num n03 = this.m03;
+    num n13 = this.m13;
+    num n33 = this.m33;
+
+    _data[00] = m00 * n00 + m01 * n10;
+    _data[01] = m10 * n00 + m11 * n10;
+    _data[03] = m30 * n00 + m31 * n10 + n30;
+    _data[04] = m00 * n01 + m01 * n11;
+    _data[05] = m10 * n01 + m11 * n11;
+    _data[07] = m30 * n01 + m31 * n11 + n31;
+    _data[08] = m00 * n02 + m01 * n12;
+    _data[09] = m10 * n02 + m11 * n12;
+    _data[11] = m30 * n02 + m31 * n12 + n32;
+    _data[12] = m00 * n03 + m01 * n13;
+    _data[13] = m10 * n03 + m11 * n13;
+    _data[15] = m30 * n03 + m31 * n13 + n33;
   }
 
   void copyFromAndConcat2D(Matrix3D copyMatrix, Matrix concatMatrix) {
