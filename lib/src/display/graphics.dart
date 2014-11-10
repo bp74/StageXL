f94864d056a33a3b2ff85ddf5f22872b49b7c5a3
marcojakob <majakob@gmx.ch>
2014-11-10 14:18:07 +0100

    Use the triple slash /// comments according to the Dart Style Guide

--- lib/src/display/graphics.dart
+++ lib/src/display/graphics.dart
@@ -1,24 +1,21 @@
 part of stagexl.display;
 
-/**
- * A vector graphics drawing surface.
- *
- * Example:
- *
- *     // draw a red circle
- *     var shape = new Shape();
- *     shape.graphics.circle(100, 100, 60);
- *     shape.graphics.fillColor(Color.Red);
- *     stage.addChild(shape);
- *
- * **Note:** Stroke and fill operations act on the *preceding* vector drawing operations.
- *
- * **Warning:** The WebGL backend does not support vector graphics yet.
- * If you want to draw Graphics display
- * objects please use the [DisplayObject.applyCache] method which renders the vector graphics
- * to a texture or do not opt-in for the WebGL renderer.
- *
- */
+/// A vector graphics drawing surface.
+///
+/// Example:
+///
+///     // draw a red circle
+///     var shape = new Shape();
+///     shape.graphics.circle(100, 100, 60);
+///     shape.graphics.fillColor(Color.Red);
+///     stage.addChild(shape);
+///
+/// **Note:** Stroke and fill operations act on the *preceding* vector drawing operations.
+///
+/// **Warning:** The WebGL backend does not support vector graphics yet.
+/// If you want to draw Graphics display
+/// objects please use the [DisplayObject.applyCache] method which renders the vector graphics
+/// to a texture or do not opt-in for the WebGL renderer.
 class Graphics {
 
   static const Map _BASE_64 = const {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,"K":10,"L":11,"M":12,"N":13,"O":14,"P":15,"Q":16,"R":17,"S":18,"T":19,"U":20,"V":21,"W":22,"X":23,"Y":24,"Z":25,"a":26,"b":27,"c":28,"d":29,"e":30,"f":31,"g":32,"h":33,"i":34,"j":35,"k":36,"l":37,"m":38,"n":39,"o":40,"p":41,"q":42,"r":43,"s":44,"t":45,"u":46,"v":47,"w":48,"x":49,"y":50,"z":51,"0":52,"1":53,"2":54,"3":55,"4":56,"5":57,"6":58,"7":59,"8":60,"9":61,"+":62,"/":63};
@@ -38,104 +35,71 @@ class Graphics {
     _boundsRefresh = true;
   }
 
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
-  /**
-   * Start drawing a freeform path.
-   *
-   */
+  /// Start drawing a freeform path.
   void beginPath() =>
     _addCommand(new _GraphicsCommandBeginPath());
 
-  /**
-   * Stop drawing a freeform path.
-   */
+  /// Stop drawing a freeform path.
   void closePath() =>
     _addCommand(new _GraphicsCommandClosePath());
 
-  /**
-   * Moves the next point in the path to [x] and [y]
-   */
+  /// Moves the next point in the path to [x] and [y]
   void moveTo(num x, num y) =>
     _addCommand(new _GraphicsCommandMoveTo(x, y));
 
-  /**
-   * From the current point in the path, draw a line to [x] and [y]
-   */
+  /// From the current point in the path, draw a line to [x] and [y]
   void lineTo(num x, num y) =>
     _addCommand(new _GraphicsCommandLineTo(x, y));
 
-  /**
-   * From the current point in the path, draw an arc to [endX] and [endY]
-   */
+  /// From the current point in the path, draw an arc to [endX] and [endY]
   void arcTo(num controlX, num controlY, num endX, num endY, num radius) =>
     _addCommand(new _GraphicsCommandArcTo(controlX, controlY, endX, endY, radius));
 
-  /**
-   * From the current point in the path, draw a quadratic curve to [endX] and [endY]
-   */
+  /// From the current point in the path, draw a quadratic curve to [endX] and [endY]
   void quadraticCurveTo(num controlX, num controlY, num endX, num endY) =>
     _addCommand(new _GraphicsCommandQuadraticCurveTo(controlX, controlY, endX, endY));
 
-  /**
-   * From the current point in the path, draw a bezier curve to [endX] and [endY]
-   */
+  /// From the current point in the path, draw a bezier curve to [endX] and [endY]
   bezierCurveTo(num controlX1, num controlY1, num controlX2, num controlY2, num endX, num endY) =>
     _addCommand(new _GraphicsCommandBezierCurveTo(controlX1, controlY1, controlX2, controlY2, endX, endY));
 
-  /**
-   * Draw an arc at [x] and [y].
-   */
+  /// Draw an arc at [x] and [y].
   void arc(num x, num y, num radius, num startAngle, num endAngle, bool antiClockwise) =>
     _addCommand(new _GraphicsCommandArc(x, y, radius, startAngle, endAngle, antiClockwise));
 
-  /**
-   * Draw a rectangle at [x] and [y]
-   */
+  /// Draw a rectangle at [x] and [y]
   void rect(num x, num y, num width, num height) =>
     _addCommand(new _GraphicsCommandRect(x,y, width, height));
 
-  /**
-   * Apply a stroke color to the **previously drawn** vector object.
-   */
+  /// Apply a stroke color to the **previously drawn** vector object.
   void strokeColor(int color, [num width = 1.0, String joints = JointStyle.ROUND, String caps = CapsStyle.ROUND]) =>
     _addCommand(new _GraphicsCommandStrokeColor(color2rgba(color), width, joints, caps));
 
-  /**
-   * Apply a stroke color to the **previously drawn** vector object.
-   */
+  /// Apply a stroke color to the **previously drawn** vector object.
   void strokeGradient(GraphicsGradient gradient, [num width = 1.0, String joints = JointStyle.ROUND, String caps = CapsStyle.ROUND]) =>
     _addCommand(new _GraphicsCommandStrokeGradient(gradient, width, joints, caps));
 
-  /**
-   * Apply a stroke pattern to the **previously drawn** vector object.
-   */
+  /// Apply a stroke pattern to the **previously drawn** vector object.
   void strokePattern(GraphicsPattern pattern, [num width = 1.0, String joints = JointStyle.ROUND, String caps = CapsStyle.ROUND]) =>
     _addCommand(new _GraphicsCommandStrokePattern(pattern, width, joints, caps));
 
-  /**
-   * Apply a fill color to the **previously drawn** vector object.
-   */
+  /// Apply a fill color to the **previously drawn** vector object.
   void fillColor(int color) =>
     _addCommand(new _GraphicsCommandFillColor(color2rgba(color)));
 
-  /**
-   * Apply a fill gradient to the **previously drawn** vector object.
-   */
+  /// Apply a fill gradient to the **previously drawn** vector object.
   void fillGradient(GraphicsGradient gradient) =>
     _addCommand(new _GraphicsCommandFillGradient(gradient));
 
-  /**
-   * Apply a fill pattern to the **previously drawn** vector object.
-   */
+  /// Apply a fill pattern to the **previously drawn** vector object.
   void fillPattern(GraphicsPattern pattern) =>
     _addCommand(new _GraphicsCommandFillPattern(pattern));
 
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
-  /**
-   * Draw a rounded rectangle at [x] and [y].
-   */
+  /// Draw a rounded rectangle at [x] and [y].
   void rectRound(num x, num y, num width, num height, num ellipseWidth, num ellipseHeight) {
 
     _addCommand(new _GraphicsCommandMoveTo(x + ellipseWidth, y));
@@ -149,22 +113,18 @@ class Graphics {
     _addCommand(new _GraphicsCommandQuadraticCurveTo(x, y, x + ellipseWidth, y));
   }
 
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
-  /**
-   * Draw a circle at [x] and [y]
-   */
+  /// Draw a circle at [x] and [y]
   void circle(num x, num y, num radius) {
 
     _addCommand(new _GraphicsCommandMoveTo(x + radius, y));
     _addCommand(new _GraphicsCommandArc(x, y, radius, 0, PI * 2, false));
   }
 
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
-  /**
-   * Draw an ellipse at [x] and [y]
-   */
+  /// Draw an ellipse at [x] and [y]
   void ellipse(num x, num y, num width, num height) {
 
     num kappa = 0.5522848;
@@ -184,7 +144,7 @@ class Graphics {
     _addCommand(new _GraphicsCommandBezierCurveTo(xm - ox, y2, x1, ym + oy, x1, ym));
   }
 
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
   void decode(String str)  {
 
@@ -223,8 +183,8 @@ class Graphics {
     }
   }
 
-  //-------------------------------------------------------------------------------------------------
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
   Rectangle<num> get bounds {
 
@@ -243,7 +203,7 @@ class Graphics {
     return _boundsRectangle.clone();
   }
 
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
   bool hitTestInput(num localX, num localY) {
 
@@ -268,7 +228,7 @@ class Graphics {
 
     return false;
   }
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
   void render(RenderState renderState) {
 
@@ -293,7 +253,7 @@ class Graphics {
     }
   }
 
-  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
   void _drawPath(CanvasRenderingContext2D context) {
 
