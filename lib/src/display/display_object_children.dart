bf8feb6b9ba96c117cb2efa61db6bb537f9a93c1
Bernhard <bernhard.robert.pichler@gmail.com>
2015-03-16 23:12:34 +0100

    Removed wrong override annotations

--- lib/src/display/display_object_children.dart
+++ lib/src/display/display_object_children.dart
@@ -78,137 +78,110 @@ class DisplayObjectChildren implements Iterable<DisplayObject> {
 
   //---------------------------------------------------------------------------
 
-  @override
   bool any(bool test(DisplayObject element)) {
     return _children.any(test);
   }
 
-  @override
   bool contains(DisplayObject element) {
     return _children.contains(element);
   }
 
-  @override
   DisplayObject elementAt(int index) {
     return _children[index];
   }
 
-  @override
   bool every(bool test(DisplayObject element)) {
     return _children.every(test);
   }
 
-  @override
   Iterable expand(Iterable f(DisplayObject element)) {
     return _children.expand(f);
   }
 
-  @override
   DisplayObject get first {
     return _children.first;
   }
 
-  @override
   DisplayObject firstWhere(bool test(DisplayObject element), {DisplayObject orElse()}) {
     return _children.firstWhere(test, orElse: orElse);
   }
 
-  @override
-  fold(initialValue, combine(previousValue, DisplayObject element)) {
-    _children.fold(initialValue, combine);
+  dynamic fold(initialValue, dynamic combine(previousValue, DisplayObject element)) {
+    return _children.fold(initialValue, combine);
   }
 
-  @override
   void forEach(void f(DisplayObject element)) {
     _children.forEach(f);
   }
 
-  @override
   bool get isEmpty {
     return _children.isEmpty;
   }
 
-  @override
   bool get isNotEmpty {
     return _children.isNotEmpty;
   }
 
-  @override
   Iterator<DisplayObject> get iterator {
     return _children.iterator;
   }
 
-  @override
   String join([String separator = ""]) {
     return _children.join(separator);
   }
 
-  @override
   DisplayObject get last {
     return _children.last;
   }
 
-  @override
   DisplayObject lastWhere(bool test(DisplayObject element), {DisplayObject orElse()}) {
     return _children.lastWhere(test, orElse: orElse);
   }
 
-  @override
   int get length {
     return _children.length;
   }
 
-  @override
   Iterable map(f(DisplayObject element)) {
     return _children.map(f);
   }
 
-  @override
   DisplayObject reduce(DisplayObject combine(DisplayObject value, DisplayObject element)) {
     return _children.reduce(combine);
   }
 
-  @override
   DisplayObject get single {
     return _children.single;
   }
 
-  @override
   DisplayObject singleWhere(bool test(DisplayObject element)) {
     return _children.singleWhere(test);
   }
 
-  @override
   Iterable<DisplayObject> skip(int count) {
     return _children.skip(count);
   }
 
-  @override
   Iterable<DisplayObject> skipWhile(bool test(DisplayObject value)) {
     return _children.skipWhile(test);
   }
 
-  @override
   Iterable<DisplayObject> take(int count) {
     return _children.take(count);
   }
 
-  @override
   Iterable<DisplayObject> takeWhile(bool test(DisplayObject value)) {
     return _children.takeWhile(test);
   }
 
-  @override
   List<DisplayObject> toList({bool growable: true}) {
     return _children.toList(growable: growable);
   }
 
-  @override
   Set<DisplayObject> toSet() {
     return _children.toSet();
   }
 
-  @override
   Iterable<DisplayObject> where(bool test(DisplayObject element)) {
     return _children.where(test);
   }
