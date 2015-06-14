2e1491f6adf9f2d05e8440f538c92b21048e8268
nilsdoehring <nilsdoehring@gmail.com>
2015-06-14 23:08:01 +0200

    Added override of render method to Sprite3D.

--- lib/src/display/sprite_3d.dart
+++ lib/src/display/sprite_3d.dart
@@ -144,6 +144,14 @@ class Sprite3D extends DisplayObjectContainer3D implements Sprite {
   //----------------------------------------------------------------------------
 
   @override
+  render(RenderState renderState) {
+    if (_graphics != null) _graphics.render(renderState);
+    super.render(renderState);
+  }
+
+  //----------------------------------------------------------------------------
+
+  @override
   void _renderWithProjection(RenderState renderState) {
     if (_graphics != null) _graphics.render(renderState);
     super._renderWithProjection(renderState);
