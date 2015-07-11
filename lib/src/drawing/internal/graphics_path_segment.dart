6e592617d2d85bf802b2aa1e6dbc957da9bf6a58
Bernhard <bernhard.robert.pichler@gmail.com>
2015-07-11 19:33:22 +0200

    Simplified graphics path triangulation

--- lib/src/drawing/internal/graphics_path_segment.dart
+++ lib/src/drawing/internal/graphics_path_segment.dart
@@ -59,6 +59,9 @@ class GraphicsPathSegment {
 
   void _triangulateVertexBuffer() {
 
+    // TODO: benchmark more triangulation methods
+    // http://erich.realtimerendering.com/ptinpoly/
+
     List<int> result = new List<int>();
     List<int> available = new List<int>();
     int index = 0;
@@ -80,61 +83,27 @@ class GraphicsPathSegment {
       num x3 = _vertexBuffer[i2 * 2 + 0];
       num y3 = _vertexBuffer[i2 * 2 + 1];
 
-      var earFound = false;
+      num y23 = y2 - y3;
+      num x32 = x3 - x2;
+      num y31 = y3 - y1;
+      num x13 = x1 - x3;
+      num det = y23 * x13 - x32 * y31;
+      num minD = det < 0.0 ? det : 0.0;
+      num maxD = det > 0.0 ? det : 0.0;
+      var earFound = true;
 
-      if ((y1 - y2) * (x3 - x2) + (x2 - x1) * (y3 - y2) >= 0) {
-
-        earFound = true;
-
-        for(int j = 0; j < available.length; j++) {
-
-          int vi = available[j];
-          if (vi == i0 || vi == i1 || vi == i2) continue;
-
-          // TODO: check if this works too:
-
-          /*
-          double y23 = y2 - y3;
-          double x32 = x3 - x2;
-          double y31 = y3 - y1;
-          double x13 = x1 - x3;
-          double det = y23 * x13 - x32 * y31;
-          double minD = Math.min(det, 0);
-          double maxD = Math.max(det, 0);
-          double dx = x - x3;
-          double dy = y - y3;
-
-          double a = y23 * dx + x32 * dy;
-          if (a < minD || a > maxD) return false;
-          double b = y31 * dx + x13 * dy;
-          if (b < minD || b > maxD) return false;
-          double c = det - a - b;
-          if (c < minD || c > maxD) return false;
-          return true;
-          */
-
-          num v0x = x3 - x1;
-          num v0y = y3 - y1;
-          num v1x = x2 - x1;
-          num v1y = y2 - y1;
-          num v2x = _vertexBuffer[vi * 2 + 0] - x1;
-          num v2y = _vertexBuffer[vi * 2 + 1] - y1;
-
-          num dot00 = v0x * v0x + v0y * v0y;
-          num dot01 = v0x * v1x + v0y * v1y;
-          num dot02 = v0x * v2x + v0y * v2y;
-          num dot11 = v1x * v1x + v1y * v1y;
-          num dot12 = v1x * v2x + v1y * v2y;
-
-          num d = (dot00 * dot11 - dot01 * dot01);
-          num u = (dot11 * dot02 - dot01 * dot12) / d;
-          num v = (dot00 * dot12 - dot01 * dot02) / d;
-
-          if ((u >= 0) && (v >= 0) && (u + v < 1)) {
-            earFound = false;
-            break;
-          }
-        }
+      for(int j = 0; j < available.length && det < 0.0 && earFound; j++) {
+        int vi = available[j];
+        if (vi == i0 || vi == i1 || vi == i2) continue;
+        num dx = _vertexBuffer[vi * 2 + 0] - x3;
+        num dy = _vertexBuffer[vi * 2 + 1] - y3;
+        num a = y23 * dx + x32 * dy;
+        if (a < minD || a > maxD) continue;
+        num b = y31 * dx + x13 * dy;
+        if (b < minD || b > maxD) continue;
+        num c = det - a - b;
+        if (c < minD || c > maxD) continue;
+        earFound = false;
       }
 
       if(earFound) {
