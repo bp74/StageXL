04f404d672588064823dd49b66075b87322f773c
Bernhard <bernhard.robert.pichler@gmail.com>
2015-10-17 20:51:11 +0200

    Some code beautifications

--- lib/src/engine/render_program.dart
+++ lib/src/engine/render_program.dart
@@ -6,13 +6,18 @@ abstract class RenderProgram {
   gl.RenderingContext _renderingContext = null;
   gl.Program _program = null;
 
-  RenderBufferIndex _renderBufferIndex = null;
-  RenderBufferVertex _renderBufferVertex = null;
+  Map<String, int> _attributes;
+  Map<String, gl.UniformLocation> _uniforms;
+  RenderBufferIndex _renderBufferIndex;
+  RenderBufferVertex _renderBufferVertex;
 
-  final Map<String, int> _attributes = new Map<String, int>();
-  final Map<String, gl.UniformLocation> _uniforms = new Map<String, gl.UniformLocation>();
+  RenderProgram()
+      : _attributes = new Map<String, int>(),
+        _uniforms = new Map<String, gl.UniformLocation>(),
+        _renderBufferIndex = new RenderBufferIndex(0),
+        _renderBufferVertex = new RenderBufferVertex(0);
 
-  //-----------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
 
   String get vertexShaderSource;
   String get fragmentShaderSource;
@@ -26,62 +31,33 @@ abstract class RenderProgram {
   Map<String, int> get attributes => _attributes;
   Map<String, gl.UniformLocation> get uniforms => _uniforms;
 
-  //-----------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
 
   void set projectionMatrix(Matrix3D matrix) {
-    renderingContext.uniformMatrix4fv(uniforms["uProjectionMatrix"], false, matrix.data);
+    var uIndex = uniforms["uProjectionMatrix"];
+    renderingContext.uniformMatrix4fv(uIndex, false, matrix.data);
   }
+
+  //---------------------------------------------------------------------------
 
   void activate(RenderContextWebGL renderContext) {
 
     if (this.contextIdentifier != renderContext.contextIdentifier) {
-
       _contextIdentifier = renderContext.contextIdentifier;
       _renderingContext = renderContext.rawContext;
-      _program = renderingContext.createProgram();
-      _attributes.clear();
-      _uniforms.clear();
-
       _renderBufferIndex = renderContext.renderBufferIndex;
-      _renderBufferIndex.activate(renderContext);
-
       _renderBufferVertex = renderContext.renderBufferVertex;
+      _renderBufferIndex.activate(renderContext);
       _renderBufferVertex.activate(renderContext);
-
-      var vertexShader = _createShader(renderingContext, vertexShaderSource, gl.VERTEX_SHADER);
-      var fragmentShader = _createShader(renderingContext, fragmentShaderSource, gl.FRAGMENT_SHADER);
-
-      renderingContext.attachShader(program, vertexShader);
-      renderingContext.attachShader(program, fragmentShader);
-      renderingContext.linkProgram(program);
-
-      var programStatus = renderingContext.getProgramParameter(program, gl.LINK_STATUS);
-      var isContextLost = renderingContext.isContextLost();
-      if (programStatus == false && isContextLost == false) {
-        throw renderingContext.getProgramInfoLog(program);
-      }
-
-      int activeAttributes = renderingContext.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
-      int activeUniforms = renderingContext.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
-
-      for(int index = 0; index < activeAttributes; index++) {
-        var activeInfo = renderingContext.getActiveAttrib(program, index);
-        var location = renderingContext.getAttribLocation(program, activeInfo.name);
-        renderingContext.enableVertexAttribArray(location);
-        _attributes[activeInfo.name] = location;
-      }
-
-      for(int index = 0; index < activeUniforms; index++) {
-        var activeInfo = renderingContext.getActiveUniform(program, index);
-        var location = renderingContext.getUniformLocation(program, activeInfo.name);
-        _uniforms[activeInfo.name] = location;
-      }
+      _program = _createProgram(_renderingContext);
+      _updateAttributes(_renderingContext, _program);
+      _updateUniforms(_renderingContext, _program);
     }
 
     renderingContext.useProgram(program);
   }
 
-  //-----------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
 
   void flush() {
     if (renderBufferIndex.position > 0 && renderBufferVertex.position > 0) {
@@ -96,21 +72,66 @@ abstract class RenderProgram {
     }
   }
 
-  //-----------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
 
-  gl.Shader _createShader(gl.RenderingContext renderingContext, String source, int shaderType) {
+  gl.Shader _createShader(gl.RenderingContext rc, String source, int type) {
 
-    var shader = renderingContext.createShader(shaderType);
+    var shader = rc.createShader(type);
+    rc.shaderSource(shader, source);
+    rc.compileShader(shader);
 
-    renderingContext.shaderSource(shader, source);
-    renderingContext.compileShader(shader);
-
-    var shaderStatus = renderingContext.getShaderParameter(shader, gl.COMPILE_STATUS);
-    var isContextLost = renderingContext.isContextLost();
-    if (shaderStatus == false && isContextLost == false) {
-      throw renderingContext.getShaderInfoLog(shader);
-    }
-
-    return shader;
+    var status = rc.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (status) return shader;
+    var cl = rc.isContextLost();
+    var error = cl ? "Error" : rc.getShaderInfoLog(shader);
+    throw new StateError(error);
   }
+
+  //---------------------------------------------------------------------------
+
+  gl.Program _createProgram(gl.RenderingContext rc) {
+
+    var program = rc.createProgram();
+    var vShader = _createShader(rc, vertexShaderSource, gl.VERTEX_SHADER);
+    var fShader = _createShader(rc, fragmentShaderSource, gl.FRAGMENT_SHADER);
+
+    rc.attachShader(program, vShader);
+    rc.attachShader(program, fShader);
+    rc.linkProgram(program);
+
+    var status = rc.getProgramParameter(program, gl.LINK_STATUS);
+    if (status) return program;
+    var cl = rc.isContextLost();
+    var error = cl ? "Error" : rc.getProgramInfoLog(program);
+    throw new StateError(error);
+  }
+
+  //---------------------------------------------------------------------------
+
+  void _updateAttributes(gl.RenderingContext rc, gl.Program program) {
+
+    _attributes.clear();
+    int count = rc.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
+    for(int index = 0; index < count; index++) {
+      var activeInfo = rc.getActiveAttrib(program, index);
+      var location = rc.getAttribLocation(program, activeInfo.name);
+      rc.enableVertexAttribArray(location);
+      _attributes[activeInfo.name] = location;
+    }
+  }
+
+  //---------------------------------------------------------------------------
+
+  void _updateUniforms(gl.RenderingContext rc, gl.Program program) {
+
+    _uniforms.clear();
+    int count = rc.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+    for(int i = 0; i < count; i++) {
+      var activeInfo = rc.getActiveUniform(program, i);
+      var location = rc.getUniformLocation(program, activeInfo.name);
+      _uniforms[activeInfo.name] = location;
+    }
+  }
+
 }
