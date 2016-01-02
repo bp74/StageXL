46872a0c2847aff218a9a91e0599796066182296
Bernhard <bernhard.robert.pichler@gmail.com>
2016-01-02 09:30:59 +0100

    Improved graphics stroke calculation

--- lib/src/drawing/internal/graphics_stroke_segment.dart
+++ lib/src/drawing/internal/graphics_stroke_segment.dart
@@ -6,78 +6,74 @@ class GraphicsStrokeSegment extends GraphicsMesh {
   final GraphicsPathSegment pathSegment;
 
   GraphicsStrokeSegment(GraphicsStroke stroke, GraphicsPathSegment pathSegment)
-      : super(pathSegment.vertexCount * 2, pathSegment.vertexCount * 6),
-        this.stroke = stroke,
-        this.pathSegment = pathSegment {
+      : this.stroke = stroke,
+        this.pathSegment = pathSegment,
+        super(pathSegment.vertexCount * 2, pathSegment.vertexCount * 6) {
 
     // TODO: implement full stroke logic!
     // joint, currently always JointStyle.MITER
     // caps, currently always CapsStyle.BUTT
-    // calculate normals and joints in one go (avoid length segments)
     // calculate correct miter limit (not infinite like now)
-    // take closePath into account
 
-    _calculateStroke();
+    if (pathSegment.vertexCount >= 2) {
+      _calculateStroke();
+    }
   }
+
+  //---------------------------------------------------------------------------
 
   void _calculateStroke() {
 
     var length = pathSegment.vertexCount;
+    var closed = pathSegment.closed;
     var vertices = pathSegment._vertexBuffer;
-    if (length < 2) return;
-
-    // calculate normals
-
-    var normals = new Float32List(length * 2);
     var width = stroke.command.width;
 
-    for (var i = 0; i < length - 1; i++) {
-      num x1 = vertices[i * 2 + 0];
-      num y1 = vertices[i * 2 + 1];
-      num x2 = vertices[i * 2 + 2];
-      num y2 = vertices[i * 2 + 3];
-      num vx = x2 - x1;
-      num vy = y2 - y1;
+    var v1x = 0.0, v1y = 0.0;
+    var n1x = 0.0, n1y = 0.0;
+    var v2x = 0.0, v2y = 0.0;
+    var n2x = 0.0, n2y = 0.0;
+
+    for (var i = -2; i <= length; i++) {
+
+      // get next vertex
+      var offset = ((i + 1) % length) * 2;
+      v2x = vertices[offset + 0];
+      v2y = vertices[offset + 1];
+
+      // calculate next normal
+      num vx = v2x - v1x;
+      num vy = v2y - v1y;
       num vl = math.sqrt(vx * vx + vy * vy);
-      normals[i * 2 + 0] = 0.0 - (width / 2.0) * (vy / vl);
-      normals[i * 2 + 1] = 0.0 + (width / 2.0) * (vx / vl);
-    }
+      n2x = 0.0 - (width / 2.0) * (vy / vl);
+      n2y = 0.0 + (width / 2.0) * (vx / vl);
 
-    // calculate joints
-
-    num n1x = 0.0;
-    num n1y = 0.0;
-
-    for (var i = 0; i < length; i++) {
-
-      num v2x = vertices[i * 2 + 0];
-      num v2y = vertices[i * 2 + 1];
-      num n2x = normals[i * 2 + 0];
-      num n2y = normals[i * 2 + 1];
-
-      if (i == 0) {
-        addVertex(v2x + n2x, v2y + n2y);
-        addVertex(v2x - n2x, v2y - n2y);
-      } else if (i == length - 1) {
-        addVertex(v2x + n1x, v2y + n1y);
-        addVertex(v2x - n1x, v2y - n1y);
-      } else {
+      // calculate vertices
+      if (i == 0 && closed == false) {
+        addVertex(v1x + n2x, v1y + n2y);
+        addVertex(v1x - n2x, v1y - n2y);
+      } else if (i == length - 1 && closed == false) {
+        addVertex(v1x + n1x, v1y + n1y);
+        addVertex(v1x - n1x, v1y - n1y);
+      } else if (i >= 0 && (i < length || closed)) {
         num id = (n2x * n1y - n2y * n1x);
         num it = (n2x * (n1x - n2x) + n2y * (n1y - n2y)) / id;
         num ix = n1x - it * n1y;
         num iy = n1y + it * n1x;
-        addVertex(v2x + ix, v2y + iy);
-        addVertex(v2x - ix, v2y - iy);
+        addVertex(v1x + ix, v1y + iy);
+        addVertex(v1x - ix, v1y - iy);
       }
 
-      n1x = n2x;
-      n1y = n2y;
-
-      var vertexCount = this.vertexCount;
-      if (vertexCount >= 4) {
+      // add indices
+      if (i > 0 && (i < length || closed)) {
+        var vertexCount = this.vertexCount;
         addTriangle(vertexCount - 4, vertexCount - 3, vertexCount - 2);
         addTriangle(vertexCount - 3, vertexCount - 2, vertexCount - 1);
       }
+
+      // shift vertices and normals
+      v1x = v2x; v1y = v2y;
+      n1x = n2x; n1y = n2y;
     }
   }
 
