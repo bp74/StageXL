ade5933ad02f4772438e197aff99c08ab43b1587
Bernhard <bernhard.robert.pichler@gmail.com>
2016-02-13 07:59:29 +0100

    Revert "Changed workaround for canvas renderer"
    This reverts commit 07d226e45aa272ab87a57b8ceab663ea2b42f095.

--- lib/src/engine/render_context_canvas.dart
+++ lib/src/engine/render_context_canvas.dart
@@ -108,12 +108,7 @@ class RenderContextCanvas extends RenderContext {
       context.globalCompositeOperation = blendMode.compositeOperation;
     }
 
-    if (matrix.det < 0.001 && matrix.det >= -0.001) {
-
-      // Workaround scaling issue on Firefox Linux
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=661452
-
-    } else if (rotation == 0) {
+    if (rotation == 0) {
 
       context.setTransform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.tx, matrix.ty);
       context.drawImageScaledFromSource(source,
