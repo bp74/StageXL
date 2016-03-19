b3fa92353320a2dd2ab23f2464be7f53c975c33e
Bernhard <bernhard.robert.pichler@gmail.com>
2016-03-19 11:44:48 +0100

    Added support for Starling JSON texture atlas

--- lib/src/resources/texture_atlas_format.dart
+++ lib/src/resources/texture_atlas_format.dart
@@ -5,7 +5,8 @@ abstract class TextureAtlasFormat {
   static const TextureAtlasFormat JSON = const _TextureAtlasFormatJson();
   static const TextureAtlasFormat JSONARRAY = const _TextureAtlasFormatJson();
   static const TextureAtlasFormat LIBGDX = const _TextureAtlasFormatLibGDX();
-  static const TextureAtlasFormat STARLING = const _TextureAtlasFormatStarling();
+  static const TextureAtlasFormat STARLINGXML = const _TextureAtlasFormatStarlingXml();
+  static const TextureAtlasFormat STARLINGJSON = const _TextureAtlasFormatStarlingJson();
 
   const TextureAtlasFormat();
 
@@ -199,9 +200,9 @@ class _TextureAtlasFormatLibGDX extends TextureAtlasFormat {
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 
-class _TextureAtlasFormatStarling  extends TextureAtlasFormat {
+class _TextureAtlasFormatStarlingXml  extends TextureAtlasFormat {
 
-  const _TextureAtlasFormatStarling();
+  const _TextureAtlasFormatStarlingXml();
 
   Future<TextureAtlas> load(TextureAtlasLoader loader) async {
 
@@ -266,3 +267,65 @@ class _TextureAtlasFormatStarling  extends TextureAtlasFormat {
     throw new FormatException("Error converting '$name' to bool.");
   }
 }
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+class _TextureAtlasFormatStarlingJson extends TextureAtlasFormat {
+
+  const _TextureAtlasFormatStarlingJson();
+
+  Future<TextureAtlas> load(TextureAtlasLoader loader) async {
+
+    var source = await loader.getSource();
+    var json = JSON.decode(source);
+    var textureAtlas = new TextureAtlas();
+
+    var imagePath = _getString(json, "imagePath", "");
+    var renderTextureQuad = await loader.getRenderTextureQuad(imagePath);
+
+    for(var subTextureMap in json["SubTexture"]) {
+
+      var name = _getString(subTextureMap, "name", "");
+      var rotation = _getBool(subTextureMap, "rotated", false) ? 1 : 0;
+
+      var frameX = _getInt(subTextureMap, "x", 0);
+      var frameY = _getInt(subTextureMap, "y", 0);
+      var frameWidth = _getInt(subTextureMap, "width", 0);
+      var frameHeight = _getInt(subTextureMap, "height", 0);
+
+      var offsetX = 0 - _getInt(subTextureMap, "frameX", 0);
+      var offsetY = 0 - _getInt(subTextureMap, "frameY", 0);
+      var originalWidth = _getInt(subTextureMap, "frameWidth", frameWidth);
+      var originalHeight = _getInt(subTextureMap, "frameHeight", frameHeight);
+
+      var textureAtlasFrame = new TextureAtlasFrame(
+          textureAtlas, renderTextureQuad, name, rotation,
+          offsetX, offsetY, originalWidth, originalHeight,
+          frameX, frameY, frameWidth, frameHeight,
+          null, null);
+
+      textureAtlas.frames.add(textureAtlasFrame);
+    }
+
+    return textureAtlas;
+  }
+
+  //---------------------------------------------------------------------------
+
+  String _getString(Map map, String name, String defaultValue) {
+    var value = map[name];
+    return value is String ? value : defaultValue;
+  }
+
+  int _getInt(Map map, String name, int defaultValue) {
+    var value = map[name];
+    return value is int ? value : defaultValue;
+  }
+
+  bool _getBool(Map map, String name, bool defaultValue) {
+    var value = map[name];
+    return value is bool ? value : defaultValue;
+  }
+}
+
