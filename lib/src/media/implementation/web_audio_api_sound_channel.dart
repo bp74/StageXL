57f68df9e80b7e80ffed5b7ba6981d88bcf5afd5
Bernhard <bernhard.robert.pichler@gmail.com>
2016-04-23 14:22:18 +0200

    Updated comment about WebAudio issue

--- lib/src/media/implementation/web_audio_api_sound_channel.dart
+++ lib/src/media/implementation/web_audio_api_sound_channel.dart
@@ -126,6 +126,7 @@ class WebAudioApiSoundChannel extends SoundChannel {
     // This is a workaround for the broken onEnded event :(
     // Replace the timer with the onEnded event as soon as possible.
     // https://code.google.com/p/chromium/issues/detail?id=349543
+    // Bug was fixed in Chrome 50, waiting for Dartium to catch up.
     time = time.clamp(0.0, _duration) * 1000.0;
     var duration = new Duration(milliseconds: time.toInt());
     _completeTimer = new Timer(duration, _onCompleteTimer);
