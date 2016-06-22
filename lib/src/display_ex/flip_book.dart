c5b9f032c0f22ff038b9f279494f9c439c1f524b
mnordine <marknordine@gmail.com>
2016-06-22 10:16:11 -0300

    Use named params

--- lib/src/display_ex/flip_book.dart
+++ lib/src/display_ex/flip_book.dart
@@ -20,8 +20,7 @@ class FlipBook extends InteractiveObject implements Animatable {
 
   //---------------------------------------------------------------------------
 
-  FlipBook(List<BitmapData> bitmapDatas, [
-      int frameRate = 30, bool loop = true]) {
+  FlipBook(List<BitmapData> bitmapDatas, {int frameRate: 30, bool loop: true}) {
 
     _bitmapDatas = bitmapDatas;
     _frameDurations = new List.filled(_bitmapDatas.length, 1.0 / frameRate);
