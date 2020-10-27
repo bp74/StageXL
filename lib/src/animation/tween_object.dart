75f8c6a41be2498b8c5b2ce51d7e8a0ffef250d4
Mark Nordine <mnordine@wooplagaming.com>
2020-10-27 11:02:53 -0300

    Make TweenObject fields non-nullable, and set to non-breaking values

--- lib/src/animation/tween_object.dart
+++ lib/src/animation/tween_object.dart
@@ -14,12 +14,16 @@ abstract class TweenObject {
 /// All display objects do implement this interface.
 
 abstract class TweenObject2D extends TweenObject {
-  num? x, y;
-  num? pivotX, pivotY;
-  num? scaleX, scaleY;
-  num? skewX, skewY;
-  num? rotation;
-  num? alpha;
+  num x = 0;
+  num y = 0;
+  num pivotX = 0;
+  num pivotY = 0;
+  num scaleX = 1;
+  num scaleY = 1;
+  num skewX = 0;
+  num skewY = 0;
+  num rotation = 0;
+  num alpha = 1;
 }
 
 /// The [TweenObject3D] class defines the interface for a class
@@ -28,10 +32,10 @@ abstract class TweenObject2D extends TweenObject {
 /// All 3D display objects do implement this interface.
 
 abstract class TweenObject3D extends TweenObject {
-  num? offsetX;
-  num? offsetY;
-  num? offsetZ;
-  num? rotationX;
-  num? rotationY;
-  num? rotationZ;
+  num offsetX = 0;
+  num offsetY = 0;
+  num offsetZ = 0;
+  num rotationX = 0;
+  num rotationY = 0;
+  num rotationZ = 0;
 }
