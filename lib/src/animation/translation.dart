1669b2112e4d5dfade0dbf51052db6cfd3725bb6
Mark Nordine <mnordine@wooplagaming.com>
2020-10-27 10:58:59 -0300

    Make _currentValue non-nullable

--- lib/src/animation/translation.dart
+++ lib/src/animation/translation.dart
@@ -24,7 +24,7 @@ class Translation implements Animatable {
   final num _targetValue;
   final TransitionFunction _transition;
 
-  num? _currentValue;
+  late num _currentValue;
   Function? _onStart;
   Function? _onUpdate;
   Function? _onComplete;
@@ -67,7 +67,7 @@ class Translation implements Animatable {
         _currentValue = _startValue + transition * (_targetValue - _startValue);
 
         if (_onUpdate != null) {
-          _onUpdate!(_roundToInt ? _currentValue!.round() : _currentValue);
+          _onUpdate!(_roundToInt ? _currentValue.round() : _currentValue);
         }
         if (_onComplete != null && _currentTime == _totalTime) {
           _onComplete!();
@@ -90,7 +90,7 @@ class Translation implements Animatable {
   /// The current value.
   ///
   /// If [roundToInt] is true, this value will always be an [int].
-  num? get currentValue => _roundToInt ? _currentValue!.round() : _currentValue;
+  num get currentValue => _roundToInt ? _currentValue.round() : _currentValue;
 
   /// The total time of this [Animation].
   num get totalTime => _totalTime;
